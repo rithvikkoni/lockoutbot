fe518b600f2862f3b01c379af5c4e17a71c06899
rithvikkoni <rithvik.knd@gmail.com>
2025-12-22 20:43:56 +0530

    deploy

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import discord
 from discord.ext import commands
-from config import BOT_TOKEN
+## from config import BOT_TOKEN
 import cflink
 import duel
+
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
